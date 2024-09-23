1aef6e5587477e124f1ac010ed903ecbb5b7c9a0
shivam Kumar <shivam2377kumar@gmail.com>
2024-09-23 19:14:28 +0530

    project showing fixed

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../custom_css/SelectedProject.css'; 
-import img1 from '../assets/img1.png'
-import img2 from '../assets/img2.png'
+import img1 from '../assets/img1.png';
+import img2 from '../assets/img2.png';
 import { Card } from 'react-bootstrap';
 
 const SelectedProject = () => {
